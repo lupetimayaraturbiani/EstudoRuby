94ed3fd15ab902a4f8c6f30ea612ce53eb299eff
lupetimayaraturbiani <mayaralupetiturbiani@gmail.com>
2021-07-22 17:52:23 -0300

    Atividades do dia 22/07

---
+++ ExerciciosAlgoritmos/ex48.rb
@@ -0,0 +1,41 @@
+txt_a1 =  -> {print "Informe a nota da primeira avaliação do aluno: "}
+txt_a1.call
+a1 = gets.chomp.to_i
+txt_a2 = -> {print "Informe a nota da segunda avaliação do aluno: "}
+txt_a2.call
+a2 = gets.chomp.to_i
+def calcularMedia(a1, a2)
+    while a1 < 0  || a2 < 0 || a1 < 0 && a2 < 0 || a1 > 10 || a2 > 10 || a1 > 10 && a2 > 10
+        puts "Os valores devem ser de 0 a 10"
+        if a1 > 10 || a1 < 0 
+            txt_a1.call
+            a1 = gets.chomp.to_i
+        elsif a2 > 10 || a2 < 0
+            txt_a2.call
+            a2 = gets.chomp.to_i
+        elsif a1 > 10 && a2 > 10 || a1 < 0 && a2 < 0 ||a1 < 0 && a2 > 10 || a1 > 10 && a2 < 0
+            txt_a1.call
+            a1 = gets.chomp.to_i
+            txt_a2.call
+            a2 = gets.chomp.to_i
+        end
+    end
+
+    media = (a1 + a2) / 2
+    
+    print "A média dele é: #{media} \n"
+
+    puts "Nova cálculo? (S/N)"
+    resp = gets.chomp
+
+    if resp == "S"
+        txt_a1.call
+        a1 = gets.chomp.to_i
+        txt_a2.call
+        a2 = gets.chomp.to_i
+        calcularMedia(a1, a2)
+    end
+end
+
+calcularMedia(a1, a2)
+ 
